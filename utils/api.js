b6bce96aea82ce3c2f9d3eb10d1e9068a01831a5
Andrea De Santo <andrea.desanto@magnolia-cms.com>
2023-07-13 14:56:18 +0200

    update

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,8 @@
 const baseUrl = process.env.NEXT_PUBLIC_NEXT_APP_MGNL_HOST;
 const spaRootNodePath = process.env.NEXT_PUBLIC_NEXT_APP_MGNL_APP_BASE;
 const pagePath = process.env.NEXT_PUBLIC_NEXT_APP_MGNL_API_PAGES;
-const pagesNavPath = process.env.NEXT_APP_MGNL_API_NAV;
-const annotionsPath = process.env.NEXT_APP_MGNL_API_ANNOTATIONS;
+const pagesNavPath = process.env.NEXT_PUBLIC_NEXT_APP_MGNL_API_NAV;
+const annotionsPath = process.env.NEXT_PUBLIC_NEXT_APP_MGNL_API_ANNOTATIONS;
 const subIdQueryString = process.env.NEXT_PUBLIC_NEXT_APP_MGNL_SUB_ID_STRING;
 
 const pagesApi = baseUrl + pagePath + spaRootNodePath + subIdQueryString;
