d19e998f5163fd69c37c90fd7acfca1589662c79
Andrea De Santo <andrea.desanto@magnolia-cms.com>
2023-07-19 13:22:56 +0200

    update

--- get-env-netlify.js
+++ get-env-netlify.js
@@ -1,8 +1,18 @@
-let envName = process.env.BRANCH.replace("env/", "");
+// let envName = process.env.BRANCH.replace("env/", "");
+
+// require("fs").writeFileSync(
+//   ".config-env",
+//   `REACT_APP_CONFIG_ENV=environments/${envName}`
+// );
+
+// console.log("get-env-netlify. write: ", envName);
+
+const currentGitBranch = require("current-git-branch");
+let envName = currentGitBranch().replace("env/", "");
 
 require("fs").writeFileSync(
   ".config-env",
   `REACT_APP_CONFIG_ENV=environments/${envName}`
 );
 
-console.log("get-env-netlify. write: ", envName);
+console.log("get-env. write: ", envName);
